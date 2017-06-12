f769fbcd89300e71e258d2976787e0de78e88c2d
Shaun Baker <shaun@sjbaker.us>
2017-06-11 20:57:44 -0500

    attemping to pull it together

--- index.js
+++ index.js
@@ -14,49 +14,49 @@ function HttpSecuritySystemAccessory(log, config) {
 	this.urls = {
 		stay: {
 			url: config.urls.stay.url,
-			body: config.urls.stay.body || ""
+			body: config.urls.stay.body || "",
+			method: "POST"
 		},
 		away: {
 			url: config.urls.away.url,
-			body: config.urls.away.body || ""
+			body: config.urls.away.body || "",
+			method: "POST"
 		},
 		night: {
 			url: config.urls.night.url,
-			body: config.urls.night.body || ""
+			body: config.urls.night.body || "",
+			method: "POST"
 		},
 		disarm: {
 			url: config.urls.disarm.url,
-			body: config.urls.disarm.body || ""
+			body: config.urls.disarm.body || "",
+			method: "POST"
 		},
 		readCurrentState: {
 			url: config.urls.readCurrentState.url,
-			body: config.urls.readCurrentState.body || ""
+			body: config.urls.readCurrentState.body || "",
+			method: "GET"
 		},
 		readTargetState: {
 			url: config.urls.readTargetState.url,
-			body: config.urls.readTargetState.body || ""
+			body: config.urls.readTargetState.body || "",
+			method: "GET"
 		}
 	};
-	
-	this.httpMethod = config["http_method"] || "GET";
+	this.httpMethod = "GET";
 	this.key = config["key"] || "";
-	this.log("setting key %s", this.key);
-	this.auth = {};
-	this.auth.username = config.username || "";
-	this.auth.password = config.password || "";
-	this.auth.immediately = config.immediately || "true";	
 	this.name = config["name"];
 }
 
 HttpSecuritySystemAccessory.prototype = {
-	httpRequest: function(url, body, callback) {
+	httpRequest: function(url, body, method, callback) {
 		request({
 			url: url,
 			headers: {
 				"Authorization": this.key
 			},
 			body: body,
-			method: this.httpMethod,
+			method: method,
 		},
 		function(error, response, body) {
 			callback(error, response, body)
@@ -81,11 +81,11 @@ HttpSecuritySystemAccessory.prototype = {
 				cfg = this.urls.disarm;
 				break;
 		}
-		
+		var method = cfg.method;
 		var url = cfg.url;
 		var body = cfg.body;
 		if (url) {
-			this.httpRequest(url, body, function(error, response, responseBody) {
+			this.httpRequest(url, body, method, function(error, response, responseBody) {
 				if (error) {
 					this.log('SetState function failed: %s', error.message);
 					callback(error);
@@ -104,37 +104,35 @@ HttpSecuritySystemAccessory.prototype = {
 		if (!url) { 
 			callback(null); 
 		}
-		
-		this.httpRequest(url, body, function(error, response, responseBody) {
+		var  method = this.urls.readCurrentState.method;
+		this.httpRequest(url, body, method, function(error, response, responseBody) {
 			if (error) {
 				this.log('GetState function failed: %s', error.message);
 				callback(error);
 			} else {
-				//var state = parseInt(responseBody);
-				var stateObj = JSON.parse(responseBody);
 
-					this.log(stateObj);
-					// TODO: WAS WORKING HERE
-					var isAlarming = stateObj.panel_alarming;
-					var isArmed = stateObj.panel_armed;
-					var isArmedStay = stateObj.panel_armed_stay;
-					var isArmedNight = false;
-					var lastmessage = stateObj.last_message_received;
-					if(lastmessage.includes("NIGHT") || lastmessage.includes("INSTANT"))
-						isArmedNight = true;
-					/* 0 = stay, 1 = away, 2 = night, 3 = disarmed */
-					if(isAlarming)
-						state = 4
-					else if(isArmed && !isArmedNight && !isArmedStay)
-						state = 1;
-					else if(isArmedNight)
-						state = 2;
-					else if(isArmedStay)
-						state = 0;
-					else
-						state = 3;
-					this.log("State is currently %s", state);
-					callback(null, state);
+				var stateObj = JSON.parse(responseBody);
+				this.log(stateObj);
+				var isAlarming = stateObj.panel_alarming;
+				var isArmed = stateObj.panel_armed;
+				var isArmedStay = stateObj.panel_armed_stay;
+				var isArmedNight = false;
+				var lastmessage = stateObj.last_message_received;
+				if(lastmessage.includes("NIGHT") || lastmessage.includes("INSTANT"))
+					isArmedNight = true;
+				/* 0 = stay, 1 = away, 2 = night, 3 = disarmed */
+				if(isAlarming)
+					state = 4
+				else if(isArmed && !isArmedNight && !isArmedStay)
+					state = 1;
+				else if(isArmedNight)
+					state = 2;
+				else if(isArmedStay)
+					state = 0;
+				else
+					state = 3;
+				this.log("State is currently %s", state);
+				callback(null, state);
 
 			}
 		}.bind(this));
