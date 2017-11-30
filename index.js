e03bef0d61a901d442f3cc1cbba5cc6147b933d0
aficustree <shaun@sjbaker.us>
2017-11-30 17:49:09 -0600

    more tweaks

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ alarmdecoderAccessory.prototype = {
 				// pausing for 2sec to see if night/immediate mode active
 					this.log('stay state detected, pausing');
 					waitUntil()
-						.interval(3000)
+						.interval(5000)
 						.times(1)
 						.condition(function() {
 							that.getCurrentState(function(nestedError, nestedState){
